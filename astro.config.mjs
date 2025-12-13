267f53ab3b44f6860be6958b760565b20394a93d
JaRoHe318 <jarohe1000@gmail.com>
2025-12-13 06:11:06 -0800

    Commiting after switching repos, hoping this doesn't break

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,6 @@
-// @ts-check
 import { defineConfig } from 'astro/config';
 
-// https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  // The Main Base Station
+  site: 'https://heatshockpineapple.com', 
+});
