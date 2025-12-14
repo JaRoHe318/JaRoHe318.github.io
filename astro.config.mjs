d323392b56a5f269276406d6037fd378031cc814
JaRoHe318 <jarohe1000@gmail.com>
2025-12-13 23:21:16 -0800

    Updated robots and sitemap handling

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,7 @@
 import { defineConfig } from 'astro/config';
+import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
-  // The Main Base Station
   site: 'https://heatshockpineapple.com', 
+  integrations: [sitemap()],
 });
